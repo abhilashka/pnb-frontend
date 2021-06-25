25f330be062ee59abfc23482489a8fa4a1f0a5d5
AkashTandekar1 <akashta@cybage.com>
2021-06-25 14:23:38 +0530

    fetching all news frontend done

---
+++ admin/src/actions/newsAction.js
@@ -0,0 +1,46 @@
+
+import {
+    NEWS_FETCH_REQUEST,
+    NEWS_FETCH_SUCCESS,
+    NEWS_FETCH_FAIL,
+    NEWS_FETCH_RESET,
+} from '../constant/newsConstant'
+
+import axios from 'axios'
+import { BASE_URL, BASE_PORT } from '../constant/base'
+
+
+
+export const getNews = () => {
+    return (dispatch) => {
+        dispatch({
+            type: NEWS_FETCH_REQUEST,
+        })
+
+        const header = {
+            headers: {
+                'Content-Type': 'application/json',
+                // token: sessionStorage['token'],
+                token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjQsImlzQWN0aXZlIjoxLCJpYXQiOjE2MjM4MjgxMjN9.DvAdMOaCXvadluspauIZxxTqRyi-KEpfMdXX6RHD-2Q'
+            },
+        }
+
+        const url = BASE_URL + BASE_PORT + '/news'
+        axios
+            .get(url, header)
+            .then((response) => {
+                dispatch({
+                    type: NEWS_FETCH_SUCCESS,
+                    payload: response.data,
+                })
+            })
+            .catch((error) => {
+                dispatch({
+                    type: NEWS_FETCH_FAIL,
+                    payload: error,
+                })
+            })
+    }
+}
+
+
---
+++ admin/src/reducer/newsReducer.js
@@ -0,0 +1,22 @@
+import {
+    NEWS_FETCH_REQUEST,
+    NEWS_FETCH_SUCCESS,
+    NEWS_FETCH_FAIL,
+    NEWS_FETCH_RESET,
+  } from '../constant/newsConstant'
+
+
+  export const fetchNewsReducer = (state = {}, action) => {
+    switch (action.type) {
+      case NEWS_FETCH_REQUEST:
+        return { loading: true }
+      case NEWS_FETCH_SUCCESS:
+        return { loading: false, response: action.payload }
+      case NEWS_FETCH_FAIL:
+        return { loading: false, error: action.payload }
+      case NEWS_FETCH_RESET:
+        return {}
+      default:
+        return state
+    }
+  }
