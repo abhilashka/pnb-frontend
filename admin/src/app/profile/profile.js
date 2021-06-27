ee7d77ba80033a4bc575588d54576dd7e5d38c8f
AkashTandekar1 <akashta@cybage.com>
2021-06-27 14:17:11 +0530

    profile update done

--- admin/src/app/profile/profile.js
+++ admin/src/app/profile/profile.js
@@ -38,13 +38,10 @@ export const Profile = ({ profile }) => {
 
     useEffect(() => { }, [error, response, loading])
 
-    const title = "k"
+    // const title = "k"
     const [formData, setFormData] = useState()
 
-    const handleOnChange = (e) => {
-        setFormData(e.target.value);
-    }
-    // setFormData(response.first_name);
+  
 
 
 
@@ -52,124 +49,106 @@ export const Profile = ({ profile }) => {
     return (
         <div>
 
-            <div>
-                <div className="page-header">
+                                        {response &&
 
-                </div>
-                <div className="row">
-                    <div className="col-md-6 grid-margin stretch-card">
-                        <div className="card">
-                            <div className="card-body">
-                                <h4 className="card-title">Default form</h4>
-                                <p className="card-description"> Basic form layout </p>
-                                <form className="forms-sample">
-                                    <Form.Group>
-                                        <label htmlFor="exampleInputUsername1">Username</label>
-                                        <Form.Control type="text" id="exampleInputUsername1" placeholder="Username" />
-                                    </Form.Group>
-                                    <Form.Group>
-                                        <label htmlFor="exampleInputEmail1">Email address</label>
-                                        <Form.Control type="email" className="form-control" id="exampleInputEmail1" placeholder="Email" />
-                                    </Form.Group>
-                                    <Form.Group>
-                                        <label htmlFor="exampleInputPassword1">Password</label>
-                                        <Form.Control type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-                                    </Form.Group>
-                                    <Form.Group>
-                                        <label htmlFor="exampleInputConfirmPassword1">Confirm Password</label>
-                                        <Form.Control type="password" className="form-control" id="exampleInputConfirmPassword1" placeholder="Password" />
-                                    </Form.Group>
-                                    <div className="form-check">
-
-                                    </div>
-
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 grid-margin stretch-card">
-                        <div className="card">
-                            <div className="card-body">
-                                <h4 className="card-title">Update Profile</h4>
-                                <form className="forms-sample">
-                                    <div className="row">
-
-                                        <Form.Group >
-                                            <label htmlFor="exampleInputUsername2" >Firstname</label>
-                                            <Form.Control type="text" className="form-control col-sm-6" id="exampleInputUsername2" placeholder="Firstname" />
-                                        </Form.Group>
-                                        <Form.Group >
-                                            <label htmlFor="exampleInputEmail2" >Lastname</label>
-
-                                            <Form.Control type="email" className="form-control col-sm-6" id="exampleInputEmail2" placeholder="Lastname" />
-                                        </Form.Group>
-
-                                    </div>
-                                    <Form.Group className="row">
-                                        <label htmlFor="exampleInputMobile" className="col-sm-3 col-form-label">Mobile</label>
-                                        <div className="col-sm-9">
-                                            <Form.Control type="text" className="form-control" id="exampleInputMobile" placeholder="Mobile number" />
-                                        </div>
-                                    </Form.Group>
-                                    <Form.Group className="row">
-                                        <label htmlFor="exampleInputPassword2" className="col-sm-3 col-form-label">Password</label>
-                                        <div className="col-sm-9">
-                                            <Form.Control type="password" className="form-control" id="exampleInputPassword2" placeholder="Password" />
-                                        </div>
-                                    </Form.Group>
-                                    <Form.Group className="row">
-                                        <label htmlFor="exampleInputConfirmPassword2" className="col-sm-3 col-form-label">Re Password</label>
-                                        <div className="col-sm-9">
-                                            <Form.Control type="password" className="form-control" id="exampleInputConfirmPassword2" placeholder="Password" />
-                                        </div>
-                                    </Form.Group>
-                                    <div className="form-check">
-                                        <label className="form-check-label text-muted">
-                                            <input type="checkbox" className="form-check-input" />
-                                            <i className="input-helper"></i>
-                      Remember me
-                    </label>
-                                    </div>
-                                    <button type="submit" className="btn btn-primary mr-2">Submit</button>
-                                    <button className="btn btn-dark">Reset</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-
-
-                </div>
+                                            response.data &&
+                                            response.data.length > 0 &&
+                                            response.data.map((note, index) => {
+                                                return (
+                                                    <div>
+                                                    <div>
+                                                        <div className="page-header">
+                                        
+                                                      
+                                                           
+                                                            <div className="col-md-6 grid-margin stretch-card">
+                                                                <div className="card">
+                                                                    <div className="card-body">
+                                                                        <h4 className="card-title">Update Profile</h4>
+                                                                        <form className="forms-sample">
+                                                                            <div className="row">
+                                        
+                                                                                <Form.Group >
+                                                                                    <label htmlFor="exampleInputUsername2" >Firstname</label>
+                                                                                    <Form.Control type="text" className="form-control col-sm-6" id="exampleInputUsername2" value={note.first_name} name="firstName" placeholder="Firstname" />
+                                                                                </Form.Group>
+                                                                                <Form.Group >
+                                                                                    <label htmlFor="exampleInputEmail2" >Lastname</label>
+                                        
+                                                                                    <Form.Control type="email" className="form-control col-sm-6" id="exampleInputEmail2" value={note.last_name} placeholder="Lastname" />
+                                                                                </Form.Group>
+                                        
+                                                                            </div>
+                                                                            <Form.Group className="row">
+                                                                                <label htmlFor="exampleInputMobile" className="col-sm-3 col-form-label">Mobile</label>
+                                                                                <div className="col-sm-9">
+                                                                                    <Form.Control type="text" className="form-control" id="exampleInputMobile" value={note.phone} placeholder="Mobile number" />
+                                                                                </div>
+                                                                            </Form.Group>
+                                                                            <Form.Group className="row">
+                                                                                <label htmlFor="exampleInputPassword2" className="col-sm-3 col-form-label">Password</label>
+                                                                                <div className="col-sm-9">
+                                                                                    <Form.Control type="password" className="form-control" id="exampleInputPassword2" value={note.passwd} placeholder="Password" />
+                                                                                </div>
+                                                                            </Form.Group>
+                                                                            <Form.Group className="row">
+                                                                                <label htmlFor="exampleInputConfirmPassword2" className="col-sm-3 col-form-label">Re Password</label>
+                                                                                <div className="col-sm-9">
+                                                                                    <Form.Control type="password" className="form-control" id="exampleInputConfirmPassword3" value={note.passwd} placeholder="Password" />
+                                                                                </div>
+                                                                            </Form.Group>
+                                                                            <Form.Group className="row">
+                                                                                <label htmlFor="exampleInputCity" className="col-sm-3 col-form-label">City</label>
+                                                                                <div className="col-sm-9">
+                                                                                    <Form.Control type="text" className="form-control" id="exampleInputCity" value={note.city} placeholder="Password" />
+                                                                                </div>
+                                                                            </Form.Group>
+                                                                            <Form.Group className="row">
+                                                                                <label htmlFor="exampleInputState" className="col-sm-3 col-form-label">State</label>
+                                                                                <div className="col-sm-9">
+                                                                                    <Form.Control type="text" className="form-control" id="exampleInputState" value={note.state} placeholder="Password" />
+                                                                                </div>
+                                                                            </Form.Group>
+                                                                            <Form.Group className="row">
+                                                                                <label htmlFor="exampleInputPinCode" className="col-sm-3 col-form-label">PinCode</label>
+                                                                                <div className="col-sm-9">
+                                                                                    <Form.Control type="number" className="form-control" id="exampleInputPinCode" value={note.pincode} placeholder="Password" />
+                                                                                </div>
+                                                                            </Form.Group>
+                                        
+                                        
+                                                                           
+                                                                            <button type="submit" className="btn btn-primary mr-2">Update</button>
+                                                                            <button className="btn btn-dark">Reset</button>
+                                                                        </form>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                        
+                                        
+                                                        </div>
+                                                    </div>
+                                        
+                                        
+                                        
+                                                </div>
+                                         
+                                                )
+                                            })}
+                                   
             </div>
 
 
-
-        </div>
+        
     )
 
-
-    Profile.propTypes = {
-        /**
-         * The user details object.
-         */
-        profile: PropTypes.object
-    };
-
-
-    Profile.defaultProps = {
-        profile: {
-            name: "Sierra Brooks",
-
-            jobTitle: "Project Manager",
-            performanceReportTitle: "Workload",
-            performanceReportValue: 74,
-            metaTitle: "Description",
-            metaValue:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio eaque, quidem, commodi soluta qui quae minima obcaecati quod dolorum sint alias, possimus illum assumenda eligendi cumque?"
-        }
-
-    }
-
-
 }
+
+
+
+
+
+
+
 
 
